ce3830d3d39620b55153c7e13d39eb7cca6aab41
Hansssbro <josu11042005@gmail.com>
2023-01-06 01:27:10 -0500

    factura

--- src/app/Components/component-a/component-a.component.ts
+++ src/app/Components/component-a/component-a.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-component-a',
   templateUrl: './component-a.component.html',
@@ -10,20 +11,38 @@ export class ComponentAComponent implements OnInit {
 
 
   constructor(private router: Router) { }
-  suma:any
-  numero:any
-  sueldos=[1700, 1600, 1900];
+  //suma:any
+  //numero:any
+  //sueldos=[1700, 1600, 1900];
+  nombre: any
+  apellido: any
+  contra: any
+  correo: any
+  num:any
+  
+
   ngOnInit(): void {
-    this.numero=3;
-    this.suma=0;
-    this.almacenar()
+    //this.numero=3;
+    //this.suma=0;
+    //this.almacenar2()
+    //this.almacenar()--
     localStorage.clear()
+    //this.navegacion()
   }
- almacenar (){
+
+  almacenar(){
+    localStorage.setItem('nombre',this.nombre)
+    localStorage.setItem('apellido',this.apellido)
+    localStorage.setItem('contra',this.contra)
+    localStorage.setItem('correo',this.correo)
+    localStorage.setItem('numero',this.num)
+  }
+ /*almacenar2 (){
     localStorage.setItem('valor_total',this.numero)
     localStorage.setItem('mensaje',"oa")
-  }
+  }*/
   navegacion (){
+    this.almacenar();
     this.router.navigate(['b']);
   }
 }
--- src/app/Components/component-b/component-b.component.ts
+++ src/app/Components/component-b/component-b.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Route,Router } from '@angular/router';
 @Component({
   selector: 'app-component-b',
   templateUrl: './component-b.component.html',
@@ -7,14 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentBComponent implements OnInit {
 
-  constructor() { }
-  resultado:any
-  mensaje:any
+  constructor(private router: Router) { }
+  
+  //resultado:any
+  //mensaje:any
+  nombre: any
+  apellido:any
+  correo:any
+  numero:any
   ngOnInit(): void {
-    this.recuperar()
+   // this.recuperar()
+    this.factura()
   }
+
+  factura (){
+this.nombre = localStorage.getItem('nombre')
+this.apellido = localStorage.getItem('apellido')
+this.correo = localStorage.getItem('correo')
+this.numero = localStorage.getItem('numero')
+  }/*
   recuperar(){
     this.resultado = localStorage.getItem('valor_total')
     this.mensaje = localStorage.getItem('mensaje')
+
+  }*/
+  navegacion2 (){
+    this.router.navigate(['']);
   }
 }
+
